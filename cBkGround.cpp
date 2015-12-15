5c844400145ab9d1c4d5728e312cb3b809faa212
Omar10022 <okhala200@caledonian.ac.uk>
2015-12-15 14:27:52 +0000

    Create cBkGround.cpp

---
+++ cBkGround.cpp
@@ -0,0 +1,26 @@
+/*
+=================
+cRocket.cpp
+- Header file for class definition - IMPLEMENTATION
+=================
+*/
+#include "cBkGround.h"
+
+void cBkGround::render()
+{
+	cSprite::render();
+}
+/*
+=================================================================
+Update the sprite position
+=================================================================
+*/
+
+void cBkGround::update()
+{
+}
+
+void cBkGround::update(float deltaTime)
+{
+
+}

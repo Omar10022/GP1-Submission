0a70b1d000d73d9ea4cc0acf0fe020da961be89a
Omar10022 <okhala200@caledonian.ac.uk>
2015-12-15 14:26:21 +0000

    Create squirrelGame.cpp

---
+++ squirrelGame.cpp
@@ -0,0 +1,11 @@
+/*
+==================================================================================
+Squirrel Game
+==================================================================================
+*/
+
+#include "asteroidsGame.h"
+
+vector<cTexture*> theGameTextures; //textures vector
+vector<cAsteroid*> theAsteroids; //spaceships vector
+vector<cBullet*> theRocketBullets; //bullets vector
